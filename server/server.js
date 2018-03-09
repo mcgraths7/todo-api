1da2232706fff5078860ce70928c43ea12cdc747
Steven McGrath <mcgrath.steven7@gmail.com>
2018-03-08 22:13:09 -0500

    Adding mongoose ORM

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+let todoSchema = new Schema({
+	text: String,
+	completed: Boolean,
+	completedAt: Number
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
+
+let todo = new Todo({
+	text: "cook dinner",
+});
+
+// todo.save().then((doc) => {
+// 	console.log('Saved todo', doc);
+// }, (e) => {
+// 	console.log('Unable to save document');
+// });
+
+todo = new Todo({
+	text: 'Feed the cats',
+	completed: true,
+	completedAt: 308
+});
+
+todo.save().then((doc) => {
+	console.log('Saved todo', doc);
+}, (e) => {
+	console.log('Unable to save document');
+});
