4c0d5041ab95f9d9a449c983492735a638c8109c
Steven McGrath <mcgrath.steven7@gmail.com>
2018-03-10 20:12:22 -0500

    Added validator for user object

--- server/db/schemas/users-schema.js
+++ server/db/schemas/users-schema.js
@@ -1,13 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose  = require('mongoose'),
+			validator = require('validator');
+
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
 	email: {
 		type: String,
-		required: true,
-		minlength: 7,
-		trim: true
-	}
+		required: [true, 'Email is required'],
+		minlength: 6,
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: `{VALUE} is not a valid email!`
+		}
+	},
+	password: {
+		type: String,
+		minlength: 8,
+		required: true
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
 
 module.exports = {userSchema};
